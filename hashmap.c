dc82b04fa73f6234893ff73b483220bd2740a846
MatanelP <75391764+MatanelP@users.noreply.github.com>
2021-05-14 17:15:06 +0300

    vector.c implemented.

--- hashmap.c
+++ hashmap.c
@@ -1,4 +1,87 @@
 //
 // Created by Matanel on 12/05/2021.
 //
+#include "hashmap.h"
 
+hashmap *hashmap_alloc (hash_func func)
+{
+  hashmap *new_hashmap = malloc (sizeof *new_hashmap);
+  vector **vectors = malloc (sizeof **vectors);
+  if (!new_hashmap || !vectors)
+    {
+      return NULL;
+    }
+  new_hashmap->size = 0;
+  new_hashmap->capacity = HASH_MAP_INITIAL_CAP;
+  new_hashmap->buckets = vectors;
+  new_hashmap->hash_func = func;
+  return new_hashmap;
+}
+
+/**
+ * Frees a hash map and the elements the hash map itself allocated.
+ * @param p_hash_map pointer to dynamically allocated pointer to hash_map.
+ */
+void hashmap_free (hashmap **p_hash_map)
+{
+  if (!p_hash_map)
+    {
+      return;
+    }
+    for (size_t i = 0 ; i < (*p_hash_map)->size ; ++i)
+      {
+        vector_free(&((*p_hash_map)->buckets[i]));
+      }
+  (*p_hash_map) = NULL;
+}
+
+/**
+ * Inserts a new in_pair to the hash map.
+ * The function inserts *new*, *copied*, *dynamically allocated* in_pair,
+ * NOT the in_pair it receives as a parameter.
+ * @param hash_map the hash map to be inserted with new element.
+ * @param in_pair a in_pair the hash map would contain.
+ * @return returns 1 for successful insertion, 0 otherwise.
+ */
+int hashmap_insert (hashmap *hash_map, const pair *in_pair);
+
+/**
+ * The function returns the value associated with the given key.
+ * @param hash_map a hash map.
+ * @param key the key to be checked.
+ * @return the value associated with key if exists, NULL otherwise (the value itself,
+ * not a copy of it).
+ */
+valueT hashmap_at (const hashmap *hash_map, const_keyT key);
+
+/**
+ * The function erases the pair associated with key.
+ * @param hash_map a hash map.
+ * @param key a key of the pair to be erased.
+ * @return 1 if the erasing was done successfully, 0 otherwise. (if key not in map,
+ * considered fail).
+ */
+int hashmap_erase (hashmap *hash_map, const_keyT key);
+
+/**
+ * This function returns the load factor of the hash map.
+ * @param hash_map a hash map.
+ * @return the hash map's load factor, -1 if the function failed.
+ */
+double hashmap_get_load_factor (const hashmap *hash_map);
+
+/**
+ * This function receives a hashmap and 2 functions, the first checks a condition on the keys,
+ * and the seconds apply some modification on the values. The function should apply the modification
+ * only on the values that are associated with keys that meet the condition.
+ *
+ * Example: if the hashmap maps char->int, keyT_func checks if the char is a capital letter (A-Z),
+ * and val_t_func multiples the number by 2, hashmap_apply_if will change the map:
+ * {('C',2),('#',3),('X',5)}, to: {('C',4),('#',3),('X',10)}, and the return value will be 2.
+ * @param hash_map a hashmap
+ * @param keyT_func a function that checks a condition on keyT and return 1 if true, 0 else
+ * @param valT_func a function that modifies valueT, in-place
+ * @return number of changed values
+ */
+int
+hashmap_apply_if (const hashmap *hash_map, keyT_func keyT_func, valueT_func valT_func);//const
--- vector.c
+++ vector.c
@@ -2,10 +2,150 @@
 // Created by Matanel on 12/05/2021.
 //
 
-#include <stdio.h>
+#include "vector.h"
 
-int main()
+vector *vector_alloc (vector_elem_cpy elem_copy_func,
+                      vector_elem_cmp elem_cmp_func,
+                      vector_elem_free elem_free_func)
 {
-  printf ("hi");
-  return 0;
+  vector *new_vector = malloc (sizeof *new_vector);
+  if (!new_vector)
+    {
+      return NULL;
+    }
+  if (!elem_free_func || !elem_copy_func || !elem_cmp_func)
+    {
+      return NULL;
+    }
+  new_vector->capacity = VECTOR_INITIAL_CAP;
+  new_vector->size = 0;
+  new_vector->data = malloc (sizeof (void *) * new_vector->capacity);
+  if (!new_vector->data)
+    {
+      free (new_vector);
+      return NULL;
+    }
+  new_vector->elem_cmp_func = elem_cmp_func;
+  new_vector->elem_copy_func = elem_copy_func;
+  new_vector->elem_free_func = elem_free_func;
+  return new_vector;
+}
+
+void vector_free (vector **p_vector)
+{
+  if (!*p_vector)
+    {
+      return;
+    }
+  for (size_t i = 0; i < (*p_vector)->size; ++i)
+    {
+      (*p_vector)->elem_free_func (&(*p_vector)->data[i]);
+    }
+  free ((*p_vector)->data);
+  free ((*p_vector));
+  *p_vector = NULL;
+}
+
+void *vector_at (const vector *vector, size_t ind)
+{
+  if (!vector || ind < 0 || ind >= vector->size)
+    {
+      return NULL;
+    }
+  return vector->data[ind];
+}
+
+int vector_find (const vector *vector, const void *value)
+{
+  if (!vector || !value)
+    {
+      return -1;
+    }
+  for (size_t i = 0; i < vector->size; ++i)
+    {
+      if (vector->elem_cmp_func (vector->data[i], value))
+        {
+          return (int) i;
+        }
+    }
+  return -1;
+}
+
+int vector_push_back (vector *vector, const void *value)
+{
+  if (!vector || !value)
+    {
+      return -1;
+    }
+
+  // TODO: what if elem_copy_func(value) returns null due to alloc failure?
+
+  vector->data[vector->size] = vector->elem_copy_func (value);
+  vector->size++;
+
+  if (VECTOR_MAX_LOAD_FACTOR < vector_get_load_factor (vector))
+    {
+      vector->capacity *= VECTOR_GROWTH_FACTOR;
+      // TODO: make sure it should be **tmp or *tmp below:
+      void **tmp = realloc (vector->data, sizeof (value) * vector->capacity);
+      if (!tmp)
+        {
+          vector->capacity /= VECTOR_GROWTH_FACTOR;
+          return -1;
+        }
+      vector->data = tmp;
+    }
+
+  return 1;
+
+}
+
+double vector_get_load_factor (const vector *vector)
+{
+  if (!vector)
+    {
+      return -1;
+    }
+  return (double) vector->size / (double) vector->capacity;
+}
+
+int vector_erase (vector *vector, size_t ind)
+{
+  if (!vector || ind < 0 || ind >= vector->size)
+    {
+      return 0;
+    }
+  vector->elem_free_func (&(vector->data[ind]));
+  for (size_t i = ind; i < vector->size; ++i)
+    {
+      vector->data[i] = vector->data[i + 1];
+    }
+  vector->size--;
+
+  if (VECTOR_MIN_LOAD_FACTOR > vector_get_load_factor (vector))
+    {
+      vector->capacity /= VECTOR_GROWTH_FACTOR;
+      // TODO: make sure it should be **tmp or *tmp below:
+      void **tmp = realloc (vector->data, sizeof (void *) * vector->capacity);
+      if (!tmp)
+        {
+          vector->capacity *= VECTOR_GROWTH_FACTOR;
+          return 0;
+        }
+      vector->data = tmp;
+    }
+  return 1;
+}
+
+void vector_clear (vector *vector)
+{
+  if (!vector)
+    {
+      return;
+    }
+  for (size_t i = 0; i < vector->size; ++i)
+    {
+      vector->elem_free_func (&(vector->data[i]));
+    }
+  vector->size = 0;
 }
